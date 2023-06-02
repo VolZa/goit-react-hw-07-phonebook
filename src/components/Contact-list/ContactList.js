b0d7b83cdd8855bd028a1bbf46bc8f4f8e764891
VolZa <vvolza@gmail.com>
2023-06-02 22:33:33 +0300

    break

--- src/components/Contact-list/ContactList.js
+++ src/components/Contact-list/ContactList.js
@@ -1,35 +1,48 @@
 // import  PropTypes from 'prop-types'
 import { Contacts, Contact } from './ContactList.styled';
-import { delContact, getContacts } from 'redux/contacts/contactsSlice';
-import { getFilter } from 'redux/filter/filterSlice';
-import { useDispatch, useSelector } from 'react-redux';
+
+// import { delContact, getContacts } from 'redux/contacts/contactsSlice';
+// import { getFilter } from 'redux/filter/filterSlice';
+// import { useDispatch, useSelector } from 'react-redux';
+import { useDeleteContactsMutation, useGetContactsQuery } from 'redux/contacts/contactsAPI';
 
 // { delContact }
 export const ContactList = () => {
-   const contacts = useSelector(getContacts);
-   const filter = useSelector(getFilter);
+   const { isLoading, data:contacts, isError } =useGetContactsQuery();
+   console.log(contacts);
+   const [deleteContacts, delInfo] = useDeleteContactsMutation(); 
+  
 
-   const dispatch = useDispatch();
-   const getFilteredContacts = () => 
-      contacts.filter(contact => 
-         contact.name.toLowerCase().includes(filter.toLowerCase()
-   ));
-   const sortedFilteredContacts = getFilteredContacts().sort((a, b) =>
-    a.name.toLowerCase().localeCompare(b.name.toLowerCase())
-  );
-   
+   // console.log('contactList= ',contactsData);
+   // const contacts = useSelector(getContacts);
+   // const filter = useSelector(getFilter);
+
+   // const dispatch = useDispatch();
+//    const getFilteredContacts = () => 
+//       contacts.filter(contact => 
+//          contact.name.toLowerCase().includes(filter.toLowerCase()
+//    ));
+//    const sortedFilteredContacts = getFilteredContacts().sort((a, b) =>
+//     a.name.toLowerCase().localeCompare(b.name.toLowerCase())
+//   );
+// {sortedFilteredContacts.map(contact => {
    return (
       <Contacts>
-         {sortedFilteredContacts.map(contact => {
+         {delInfo.isLoading&&<h2>Deleting...</h2>}
+         { isLoading && (
+            <div>...Завантаження</div>
+         )}
+         {contacts.map(contact => {
             return(
                <Contact key={contact.id}>
                   <span>{contact.name}: </span>
-                  <span>{contact.number}</span>
+                  <span>{contact.phone}</span>
                   <button type="button" onClick={
-                     ()=>dispatch(delContact(contact.id))} >Delete</button>
+                     ()=>deleteContacts(contact.id)} >Delete</button>
                </Contact>
             );
-         })}        
+         })} 
+         {isError && <h2>error</h2>}        
       </Contacts>
    );
 };
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 //Файл оголошеня стор Redux
-import { persistStore, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist'
+// import { persistStore, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist'
+import { persistStore } from 'redux-persist'
 import { configureStore } from '@reduxjs/toolkit';
 
-import { storeContactsReducer } from './contacts/contactsSlice';
+// import { storeContactsReducer } from './contacts/contactsSlice';
 import initContacts from "./constants";
 import { filterSlice } from './filter/filterSlice';
+import { contactsApi } from './contacts/contactsAPI';
 
 export const initialState = {
    contacts: initContacts,
@@ -12,15 +14,16 @@ export const initialState = {
 
 export const store = configureStore({
    reducer: {
-      contacts: storeContactsReducer,
+      // contacts: storeContactsReducer,
+      // Add the generated reducer as a specific top-level slice
+      [contactsApi.reducerPath]: contactsApi.reducer,
+     
       filter: filterSlice.reducer,
    },
+
+   //middleware concat from contactsApi
    middleware: getDefaultMiddleware =>
-   getDefaultMiddleware({
-     serializableCheck: {
-       ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-     },
-   }),
+      getDefaultMiddleware().concat(contactsApi.middleware),
    
 });
 
